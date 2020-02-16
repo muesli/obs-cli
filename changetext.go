1f4e989d9b837531f1655f8ca8dbaf15530036a3
Christian Muehlhaeuser <muesli@gmail.com>
2020-02-16 21:10:22 +0100

    Support changing texts and controlling scene items

---
+++ changetext.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+
+	obsws "github.com/muesli/go-obs-websocket"
+	"github.com/spf13/cobra"
+)
+
+var changeTextCmd = &cobra.Command{
+	Use:   "change-text",
+	Short: "Changes a text label",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 2 {
+			return errors.New("change-text requires a source and the new text")
+		}
+		return changeLabel(args[0], args[1])
+	},
+}
+
+func changeLabel(source string, text string) error {
+	req := obsws.NewGetTextFreetype2PropertiesRequest(source)
+	resp, err := req.SendReceive(*client)
+	if err != nil {
+		return err
+	}
+
+	chreq := obsws.NewSetTextFreetype2PropertiesRequest(
+		source,
+		resp.Color1,
+		resp.Color2,
+		resp.CustomWidth,
+		resp.DropShadow,
+		resp.Font,
+		resp.FontFace,
+		resp.FontFlags,
+		resp.FontSize,
+		resp.FontStyle,
+		resp.FromFile,
+		resp.LogMode,
+		resp.Outline,
+		text,
+		resp.TextFile,
+		resp.WordWrap)
+	return chreq.Send(*client)
+}
+
+func init() {
+	rootCmd.AddCommand(changeTextCmd)
+}
---
+++ sceneitems.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+
+	obsws "github.com/muesli/go-obs-websocket"
+	"github.com/spf13/cobra"
+)
+
+var (
+	listSceneItemsCmd = &cobra.Command{
+		Use:   "list-sceneitems",
+		Short: "Lists all scene-items of a source",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("list-sceneitems requires a scene")
+			}
+			return listSceneItems(args[0])
+		},
+	}
+
+	toggleSceneItemCmd = &cobra.Command{
+		Use:   "toggle-sceneitem",
+		Short: "Toggles visibility of a scene-item",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 2 {
+				return errors.New("toggle-sceneitem requires a scene and scene-item")
+			}
+			return toggleSceneItem(args[0], args[1:]...)
+		},
+	}
+
+	showSceneItemCmd = &cobra.Command{
+		Use:   "show-sceneitem",
+		Short: "Makes a scene-item visible",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 2 {
+				return errors.New("show-sceneitem requires a scene and scene-item(s)")
+			}
+			return setSceneItemVisible(true, args[0], args[1:]...)
+		},
+	}
+
+	hideSceneItemCmd = &cobra.Command{
+		Use:   "hide-sceneitem",
+		Short: "Hides a scene-item",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 2 {
+				return errors.New("hide-sceneitem requires a scene and scene-item(s)")
+			}
+			return setSceneItemVisible(false, args[0], args[1:]...)
+		},
+	}
+)
+
+func listSceneItems(scene string) error {
+	req := obsws.NewGetSceneListRequest()
+	resp, err := req.SendReceive(*client)
+	if err != nil {
+		return err
+	}
+
+	for _, v := range resp.Scenes {
+		if v["name"] != scene {
+			continue
+		}
+
+		for _, s := range v["sources"].([]interface{}) {
+			src := s.(map[string]interface{})
+			fmt.Println(src["name"])
+		}
+	}
+
+	return nil
+}
+
+func setSceneItemVisible(visible bool, scene string, items ...string) error {
+	for _, item := range items {
+		req := obsws.NewGetSceneItemPropertiesRequest(scene, item)
+		resp, err := req.SendReceive(*client)
+		if err != nil {
+			return err
+		}
+
+		chreq := obsws.NewSetSceneItemPropertiesRequest(
+			scene,
+			item,
+			resp.PositionX,
+			resp.PositionY,
+			resp.PositionAlignment,
+			resp.Rotation,
+			resp.ScaleX,
+			resp.ScaleY,
+			resp.CropTop,
+			resp.CropBottom,
+			resp.CropLeft,
+			resp.CropRight,
+			visible,
+			resp.BoundsType,
+			resp.BoundsAlignment,
+			resp.BoundsX,
+			resp.BoundsY)
+		err = chreq.Send(*client)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func toggleSceneItem(scene string, items ...string) error {
+	for _, item := range items {
+		req := obsws.NewGetSceneItemPropertiesRequest(scene, item)
+		resp, err := req.SendReceive(*client)
+		if err != nil {
+			return err
+		}
+
+		err = setSceneItemVisible(!resp.Visible, scene, item)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func init() {
+	rootCmd.AddCommand(listSceneItemsCmd)
+	rootCmd.AddCommand(toggleSceneItemCmd)
+	rootCmd.AddCommand(showSceneItemCmd)
+	rootCmd.AddCommand(hideSceneItemCmd)
+}
