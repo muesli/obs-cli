7185fda456c7ee6fb559b0d7df53b41b0e5c0d7c
Marc Nickert <git@marcnickert.de>
2021-02-06 20:20:03 +0000

    ADD preview Command

---
+++ previewscene.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"errors"
+	"strings"
+
+	obsws "github.com/muesli/go-obs-websocket"
+	"github.com/spf13/cobra"
+)
+
+var previewSceneCmd = &cobra.Command{
+	Use:   "preview-scene",
+	Short: "Preview to a different OBS scene",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("preview-scene requires a scene name as argument")
+		}
+		return previewScene(strings.Join(args, " "))
+	},
+}
+
+func previewScene(scene string) error {
+	req := obsws.NewSetPreviewSceneRequest(scene)
+	return req.Send(*client)
+}
+
+func init() {
+	rootCmd.AddCommand(previewSceneCmd)
+}
